11884a51017ce21d01b6d8be98574de95b77b16e
Timescout <timepolicescout@gmail.com>
2026-07-23 11:23:03 -0700

    Moves assets into subfolder.

--- src/data/card/Card.cs
+++ src/data/card/Card.cs
@@ -12,26 +12,26 @@ public partial class Card : Control
 	public override void _Ready()
 	{
 		// Randomly Create a card
-		Attributes.Number = GD.RandRange(1, 10);
-		Attributes.CardSuit = (Suit)GD.RandRange(0, 3);
+		Attributes.Rank = GD.RandRange(1, 10);
+		Attributes.Suit = (Suit)GD.RandRange(0, 3);
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		// Set Graphics
-		GetNode<RichTextLabel>("Number").Text = "[color=black]" + Attributes.Number.ToString() + "[/color]";
-		switch (Attributes.CardSuit) {
+		GetNode<RichTextLabel>("Number").Text = "[color=black]" + Attributes.Rank.ToString() + "[/color]";
+		switch (Attributes.Suit) {
 			case Suit.Square:
-				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/Square.png");
+				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/assets/Square.png");
 				break;
 			case Suit.Circle:
-				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/Circle.png");
+				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/assets/Circle.png");
 				break;
 			case Suit.Triangle:
-				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/Triangle.png");
+				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/assets/Triangle.png");
 				break;
 			case Suit.Cross:
-				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/Cross.png");
+				GetNode<TextureRect>("Suit").Texture = GD.Load<Texture2D>("res://src/data/card/assets/Cross.png");
 				break;
 			default:
 				break;
