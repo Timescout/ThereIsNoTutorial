588b7d39e29219a0fa09fe17a842fe4f2b6f68d4
Timescout <timepolicescout@gmail.com>
2026-07-24 12:10:59 -0700

    Adds Main Menu.

--- src/core/Main.cs
+++ src/core/Main.cs
@@ -6,11 +6,38 @@ public partial class Main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		AddMainMenu();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void AddMainMenu()
+	{
+		// when the game starts, open the main menu
+		MainMenu menu = GD.Load<PackedScene>("res://src/ui/mainMenu/main_menu.tscn").Instantiate() as MainMenu;
+		menu.Name = "MainMenu";
+
+		menu.GetNode<Button>("MenuButtonContainer/QuitButton").Pressed += () => 
+		{ 
+			GetTree().Quit();
+		};
+
+		menu.GetNode<Button>("MenuButtonContainer/StartButton").Pressed += () =>
+		{
+			AddHand();
+			GetNode<MainMenu>("MainMenu").QueueFree();
+		};
+
+		AddChild(menu);
+	}
+
+	private void AddHand()
+	{
+		Hand hand = GD.Load<PackedScene>("res://src/data/Hand/hand.tscn").Instantiate() as Hand;
+		hand.Name = "Hand";
+		AddChild(hand);
 	}
 }
